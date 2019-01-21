e58e24b86ce1610a272b093553ab1dc05e676d59
Max <alemaxona@bk.ru>
2019-01-21 16:53:39 +0300

    Changed hw 13.

--- hw13/config.py
+++ hw13/config.py
@@ -1,9 +1,12 @@
 from os import urandom
 
 key = urandom(15)
+# path_db = r'sqlite:///C:/sqlite/db_flask.db'  # win
+
 
 class Configuration:
     DEBUG = True
     SECRET_KEY = key
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/alemaxona/Documents/Projects/venv/db_flask/db_flask.db'
+    # SQLALCHEMY_DATABASE_URI = path_db
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/alemaxona/Documents/Projects/venv/db_flask/db_flask.db'  # mac
